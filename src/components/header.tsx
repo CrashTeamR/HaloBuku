2108ffb5864ce42e233e9bea360b857b8611d8ff
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 02:45:43 +0800

    Update header.tsx
    Create simple header content in the Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,23 @@
+import { logo, shoppingBag } from "../../public/images";
+import Image from "next/image";
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className=" shadow-sm p-4">
+      <div className="container m-auto flex justify-between items-cente">
+        <Image
+          src={logo}
+          alt={"Brand Logo"}
+          width={74}
+          height={64}
+          className={"cursor-pointer"}
+        />
+        <Image
+          src={shoppingBag}
+          alt={"Shopping Bag Icon"}
+          className={"cursor-pointer"}
+        />
+      </div>
+    </header>
+  );
 };
