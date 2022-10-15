b141a6954946ad11d2e2c5df0c2aaf85c1359ef9
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 03:26:03 +0800

    Update header.tsx
    - Import searchIcon
    - Add form and input type search inside header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,32 @@
-import { blueLogo, shoppingBag } from "../../public/images";
+import { blueLogo, shoppingBag, searchIcon } from "../../public/images";
 import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className=" shadow-sm p-4">
-      <div className="container m-auto flex justify-between items-cente">
+    <header className="shadow-sm p-4">
+      <div className="container m-auto flex justify-between items-center">
         <Image
           src={blueLogo}
-          alt={"Brand Logo"}
+          alt="Brand Logo"
           width={74}
           height={64}
-          className={"cursor-pointer"}
+          className="cursor-pointer"
         />
+        <form className="">
+          <input
+            type="search"
+            name="search"
+            id="search"
+            className="appearance-none focus:outline-none p-2"
+          />
+          <label htmlFor="search">
+            <Image src={searchIcon} alt="Search Icon" />
+          </label>
+        </form>
         <Image
           src={shoppingBag}
-          alt={"Shopping Bag Icon"}
-          className={"cursor-pointer"}
+          alt="Shopping Bag Icon"
+          className="cursor-pointer"
         />
       </div>
     </header>
