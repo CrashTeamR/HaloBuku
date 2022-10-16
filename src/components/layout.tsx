d811210ac65af33729516102f97e0faf1c857a59
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 16:28:46 +0800

    Update index.tsx
    Add main tag for wrap the children elements

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col">
       <Header />
-      {children}
+      <main className="container mx-auto px-2 mt-5 mb-20">{children}</main>
       <Footer />
     </div>
   );
