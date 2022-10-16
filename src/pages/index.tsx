f7487a7b4dcf2cfba1c4ddfc07d8d9abb48cd10a
Denny Setyawan Huda <dennshgit@gmail.com>
2022-10-16 10:26:57 +0700

    feat: add card to root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { RootLayout } from "../components";
+import { Card, RootLayout } from "../components";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,9 +7,20 @@ export default function Home() {
       <Head>
         <title>Home | Halo Buku</title>
       </Head>
+      
 
       <RootLayout>
-        <main className="">Typescript gokil!</main>
+        <main className="container mx-auto px-2 my-5">
+          <div className="my-10">
+            <h1 className="text-3xl font-bold text-blue-800">Buku Rilisan Terbaru</h1>
+          </div>
+          <div className="flex justify-between items-center gap-3 flex-wrap">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </main>
       </RootLayout>
     </>
   );
