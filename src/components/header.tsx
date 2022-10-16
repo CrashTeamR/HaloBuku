05e29222f90616487b858510d3a3e9a52e624074
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 14:15:42 +0800

    Update header.tsx
    Update content for search bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,9 @@ export const Header = () => {
           className="cursor-pointer"
         />
         <form className="bg-gray-100 py-2 px-5 rounded-lg flex justify-between items-center gap-2 w-1/3 outline outline-1 outline-blue-200">
+          <label htmlFor="search-keyword">
+            <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
+          </label>
           <input
             type="text"
             name="search-keyword"
@@ -20,9 +23,6 @@ export const Header = () => {
             placeholder="Search title, author, or keyword ..."
             className="appearance-none focus:outline-none p-2 placeholder:text-xs w-full placeholder:text-blue-600/40 bg-transparent text-gray-800"
           />
-          <label htmlFor="search-keyword">
-            <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
-          </label>
         </form>
         <Image
           src={shoppingBag}
