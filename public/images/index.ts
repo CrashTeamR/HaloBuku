62cafcb2c11deef0354156b26cfd40a09d27e1dc
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 14:10:36 +0800

    Update index.ts

--- public/images/index.ts
+++ public/images/index.ts
@@ -2,9 +2,20 @@ import blueLogo from "./blue-logo.png";
 import whiteLogo from "./white-logo.png";
 import searchIcon from "./search-icon.svg";
 import shoppingBag from "./shopping-bag.svg";
-import examplePicture from "./example-picture.png"
+import examplePicture from "./example-picture.png";
 import happyBook from "./happy-book.png";
 import leftArrow from "./left-arrow.svg";
 import rightArrow from "./right-arrow.svg";
+import basicRightArrow from "./basic-right-arrow.svg";
 
-export { blueLogo, whiteLogo, shoppingBag, searchIcon, examplePicture, happyBook, leftArrow, rightArrow };
+export {
+  blueLogo,
+  whiteLogo,
+  shoppingBag,
+  searchIcon,
+  examplePicture,
+  happyBook,
+  leftArrow,
+  rightArrow,
+  basicRightArrow,
+};
