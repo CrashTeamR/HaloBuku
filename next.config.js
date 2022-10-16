2daa04d8f131a2d6f15907a99eb44c3c366a5bc6
Eric Pradana <ericprd_@outlook.com>
2022-10-16 21:12:36 +0700

    add domain images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images-na.ssl-images-amazon.com"],
+    domains: ["images-na.ssl-images-amazon.com", "s.s-bol.com"],
   },
 };
 
