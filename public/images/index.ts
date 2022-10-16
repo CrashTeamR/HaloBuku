67fb9bd8c9a0ba57183b4c45215a87b63266b319
Denny Setyawan Huda <dennshgit@gmail.com>
2022-10-16 10:25:49 +0700

    chore: import example picture

--- public/images/index.ts
+++ public/images/index.ts
@@ -2,5 +2,6 @@ import blueLogo from "./blue-logo.png";
 import whiteLogo from "./white-logo.png";
 import searchIcon from "./search-icon.svg";
 import shoppingBag from "./shopping-bag.svg";
+import examplePicture from "./example-picture.png"
 
-export { blueLogo, whiteLogo, shoppingBag, searchIcon };
+export { blueLogo, whiteLogo, shoppingBag, searchIcon, examplePicture };
