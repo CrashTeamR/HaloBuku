872d2cdb01e7f429841f26514add83465185a9f4
ersankarimi <ersankarimii311@gmail.com>
2022-10-16 16:52:39 +0800

    Update header.tsx
    Update classes style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,10 @@ export const Header = () => {
           className="cursor-pointer"
         />
         <form className="bg-gray-100 py-2 px-5 rounded-lg flex justify-between items-center gap-2 w-1/3 outline outline-1 outline-blue-200">
-          <label htmlFor="search-keyword">
+          <label
+            htmlFor="search-keyword"
+            className="cursor-not-allowed opacity-50"
+          >
             <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
           </label>
           <input
@@ -21,13 +24,14 @@ export const Header = () => {
             name="search-keyword"
             id="search-keyword"
             placeholder="Search title, author, or keyword ..."
-            className="appearance-none focus:outline-none p-2 placeholder:text-xs w-full placeholder:text-blue-600/40 bg-transparent text-gray-800"
+            className="appearance-none focus:outline-none p-2 placeholder:text-xs w-full placeholder:text-blue-600/40 bg-transparent text-gray-800 cursor-not-allowed disabled:opacity-50"
+            disabled
           />
         </form>
         <Image
           src={shoppingBag}
           alt="Shopping Bag Icon"
-          className="cursor-pointer"
+          className="cursor-not-allowed opacity-50"
         />
       </div>
     </header>
