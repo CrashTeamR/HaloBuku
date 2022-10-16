4e08c8810f7d63a298473d7ead823fc830e46939
Denny Setyawan Huda <dennshgit@gmail.com>
2022-10-16 11:47:33 +0700

    chore: import svg icons

--- public/images/index.ts
+++ public/images/index.ts
@@ -3,5 +3,8 @@ import whiteLogo from "./white-logo.png";
 import searchIcon from "./search-icon.svg";
 import shoppingBag from "./shopping-bag.svg";
 import examplePicture from "./example-picture.png"
+import happyBook from "./happy-book.png";
+import leftArrow from "./left-arrow.svg";
+import rightArrow from "./right-arrow.svg";
 
-export { blueLogo, whiteLogo, shoppingBag, searchIcon, examplePicture };
+export { blueLogo, whiteLogo, shoppingBag, searchIcon, examplePicture, happyBook, leftArrow, rightArrow };
