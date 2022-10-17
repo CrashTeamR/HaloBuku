a078f235c4e95e2e9b6b23e0a7befca8cef0f54e
Eric Pradana <ericprd_@outlook.com>
2022-10-18 03:17:35 +0700

    reduce header's size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 z-10 min-w-full bg-gray-200 p-4 shadow-sm">
+    <header className="fixed top-0 z-10 min-w-full bg-gray-200 p-[0.5rem_2rem] shadow-md">
       <div className="container m-auto flex items-center justify-between">
         <Link href="/">
           <a>
             <Image
               src={blueLogo}
               alt="Brand Logo"
-              width={74}
-              height={64}
+              width={54}
+              height={44}
               className="cursor-pointer"
             />
           </a>
@@ -20,7 +20,7 @@ export const Header = () => {
         <form className="flex w-1/3 items-center justify-between gap-2 rounded-lg bg-gray-100 py-2 px-5 outline outline-1 outline-blue-200">
           <label
             htmlFor="search-keyword"
-            className="cursor-not-allowed opacity-50"
+            className="flex cursor-not-allowed items-center opacity-50"
           >
             <Image src={searchIcon} alt="Search Icon" width={24} height={24} />
           </label>
@@ -29,7 +29,7 @@ export const Header = () => {
             name="search-keyword"
             id="search-keyword"
             placeholder="Telusuri judul, penulis, or kata kunci ..."
-            className="w-full cursor-not-allowed appearance-none bg-transparent p-2 text-gray-800 placeholder:text-xs placeholder:text-blue-600/40 focus:outline-none disabled:opacity-50"
+            className="w-full cursor-not-allowed appearance-none bg-transparent p-[1px] text-gray-800 placeholder:text-xs placeholder:text-blue-600/40 focus:outline-none disabled:opacity-50"
             disabled
           />
         </form>
