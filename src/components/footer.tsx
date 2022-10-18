044d5b90e470d09126497dc19fa853621ba3e9d1
ersankarimi <ersankarimii311@gmail.com>
2022-10-18 21:26:42 +0800

    Format code

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Footer = () => {
   return (
     <footer className="bg-blue-800 p-4 text-gray-100">
-      <div className="container m-auto flex justify-between items-center">
+      <div className="container m-auto flex items-center justify-between">
         <Image src={whiteLogo} alt="Brand Logo" width={64} height={54} />
         <h6>&#169; 2022 Halo Buku. All Rights Reserved.</h6>
       </div>
