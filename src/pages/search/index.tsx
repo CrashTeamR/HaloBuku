aec2b8fbfcd331edcfae64cd1ff01ba686fc0aa9
ersankarimi <ersankarimii311@gmail.com>
2022-10-20 02:54:10 +0800

    feat : show book data from search results
    Create a search page that displays books from previous search values or keywords.
    
    Book data is obtained from `getServerSideProps` which fetches data via API and filters it from search values or keywords in the search field.
    
    Then the data is returned and will be rendered during the Next.js `hydration` process via props in the SearchPage component

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,60 @@
+import type { Books } from "../../types";
+import { Layout, Card } from "../../components";
+
+interface SearchProps {
+  data: {
+    books: Books;
+  };
+}
+
+export default function SearchPage({ data }: SearchProps) {
+  const { books } = data;
+  return (
+    <Layout>
+      <div className="m-auto flex flex-col items-start justify-center px-8">
+        <div
+          className={`m-auto ${
+            !books.length ? "grid h-[40vh]" : "flex flex-col"
+          } `}
+        >
+          <div className="my-10  w-full">
+            <h1 className="my-4 text-center text-3xl font-bold text-blue-800 xl:text-left">
+              {books.length ? "Hasil Pencarian" : ""}
+            </h1>
+          </div>
+          <div className="mx-auto flex w-full flex-wrap items-center justify-center gap-8 md:max-w-screen-lg xl:justify-start">
+            {!books.length ? (
+              <div className="flex flex-col text-center ">
+                <h2 className="text-xl font-bold">Tidak ditemukan hasil</h2>
+                <h3 className="text-xs md:text-base">
+                  Coba kata kunci lain atau hapus filter penelusuran
+                </h3>
+              </div>
+            ) : (
+              books.map((book) => {
+                return <Card key={book._id} data={book} />;
+              })
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const res = await fetch(`${URL}/books`);
+  const data = await res.json();
+
+  const filteredBooks = !query.q
+    ? []
+    : data.books.filter((book) => {
+        return book.title.toLowerCase().includes(query.q);
+      });
+
+  return {
+    props: { data: { books: filteredBooks } },
+  };
+}
