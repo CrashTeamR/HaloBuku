d3bb521eb62fbedf85fce48c3977a4266c61eff4
M Haidar Hanif <git@mhaidarhanif.com>
2022-10-22 21:33:05 +0700

    feat: isError & isLoading in login form submit button

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,43 +6,53 @@ import { useRouter } from "next/router";
 export default function Login() {
   const router = useRouter();
 
-  const [input, setInput] = useState({
+  const [payload, setPayload] = useState({
     email: "",
     password: "",
   });
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onChangeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     switch (name) {
       case "email":
-        setInput({ ...input, email: value });
+        setPayload({ ...payload, email: value });
         break;
       case "password":
-        setInput({ ...input, password: value });
+        setPayload({ ...payload, password: value });
         break;
       default:
-        return input;
+        return payload;
     }
   };
 
   const onSubmitHandler = async (event) => {
-    event.preventDefault();
-
-    const payload = { ...input };
-
     try {
+      setIsLoading(true);
+      event.preventDefault();
+
       const req = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/login`,
         payload
       );
 
       const { token } = req.data;
+
       localStorage.setItem("token", token);
-      if (req.status === 200) router.push("/");
+
+      if (req.status === 200) {
+        router.push("/");
+      }
     } catch (error) {
-      console.log(error);
-      document.querySelector("#masuk").classList.add("hidden");
+      console.error(error);
+      setIsError(true);
+
+      // Unnecessary
+      // document.querySelector("#masuk").classList.add("hidden");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -62,7 +72,7 @@ export default function Login() {
               className="border-b border-gray-400/60 outline-none"
               onChange={onChangeHandler}
               placeholder="Masukkan email"
-              value={input.email}
+              value={payload.email}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -76,11 +86,13 @@ export default function Login() {
               id="password"
               className="border-b border-gray-400/60 outline-none"
               onChange={onChangeHandler}
-              value={input.password}
+              value={payload.password}
             />
           </div>
           <div className="flex justify-center">
-            <SubmitButton buttonText="Masuk" />
+            <SubmitButton isError={isError} isLoading={isLoading}>
+              Masuk
+            </SubmitButton>
           </div>
         </form>
       </div>
