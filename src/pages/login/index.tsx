aa6ecb1f39c8e9e710c5eaab4cdaf9bf2df1dcc6
ersankarimi <ersankarimii311@gmail.com>
2022-10-22 23:02:45 +0800

    feat : add Head next js to chage the title

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SubmitButton from "../../components/submit-button";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Login() {
   const router = useRouter();
@@ -48,54 +49,57 @@ export default function Login() {
     } catch (error) {
       console.error(error);
       setIsError(true);
-
-      // Unnecessary
-      // document.querySelector("#masuk").classList.add("hidden");
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <section className="flex h-screen w-full items-center justify-center p-8">
-      <div className="flex flex-col gap-8 rounded-lg bg-white  p-[2rem_3rem] shadow-xl lg:w-[30%] xl:w-[28%] 2xl:w-[25%]">
-        <h1 className="text-center text-2xl font-bold ">Masuk</h1>
-        <form className="flex flex-col gap-8" onSubmit={onSubmitHandler}>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="font-semibold">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="border-b border-gray-400/60 outline-none"
-              onChange={onChangeHandler}
-              placeholder="Masukkan email"
-              value={payload.email}
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="font-semibold">
-              Password{" "}
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Masukkan password"
-              id="password"
-              className="border-b border-gray-400/60 outline-none"
-              onChange={onChangeHandler}
-              value={payload.password}
-            />
-          </div>
-          <div className="flex justify-center">
-            <SubmitButton isError={isError} isLoading={isLoading}>
-              Masuk
-            </SubmitButton>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Masuk | Halo Buku</title>
+      </Head>
+
+      <section className="flex h-screen w-full items-center justify-center p-8">
+        <div className="flex flex-col gap-8 rounded-lg bg-white  p-[2rem_3rem] shadow-xl lg:w-[30%] xl:w-[28%] 2xl:w-[25%]">
+          <h1 className="text-center text-2xl font-bold ">Masuk</h1>
+          <form className="flex flex-col gap-8" onSubmit={onSubmitHandler}>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="email" className="font-semibold">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="border-b border-gray-400/60 outline-none"
+                onChange={onChangeHandler}
+                placeholder="Masukkan email"
+                value={payload.email}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password" className="font-semibold">
+                Password{" "}
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Masukkan password"
+                id="password"
+                className="border-b border-gray-400/60 outline-none"
+                onChange={onChangeHandler}
+                value={payload.password}
+              />
+            </div>
+            <div className="flex justify-center">
+              <SubmitButton isError={isError} isLoading={isLoading}>
+                Masuk
+              </SubmitButton>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
