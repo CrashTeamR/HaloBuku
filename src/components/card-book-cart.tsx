91f279bb93634b8ef0eb15345a60b82aa7e1dd47
ersankarimi <ersankarimii311@gmail.com>
2022-10-23 18:41:08 +0800

    refactor: content of CardBookCart component

--- src/components/card-book-cart.tsx
+++ src/components/card-book-cart.tsx
@@ -1,21 +1,33 @@
 export const CardBookCart = ({ data }) => {
   return (
     <>
-      <div className="flex max-h-52 w-full flex-col items-center gap-2 p-2">
+      <div className="container flex h-60  items-center  gap-4   border-b-2 border-black/10 py-12">
         <img
           src={data.image}
-          className="bg-cover object-contain sm:h-56"
+          className=" h-full bg-cover object-contain"
           alt={`${data.title} Cover`}
         />
-        <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-bold">{data.title}</h1>
-          <h2 className="text-2xl font-bold tracking-tight text-orange-500">
-            {new Intl.NumberFormat("id-ID", {
-              style: "currency",
-              currency: "IDR",
-              maximumSignificantDigits: 3,
-            }).format(data.price)}
-          </h2>
+
+        <div className="flex w-full justify-between gap-8 ">
+          <div>
+            <h1 className=" text-lg font-bold text-blue-800 lg:text-2xl">
+              {data.title}
+            </h1>
+            <h3 className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              {data.author}
+            </h3>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h4 className="my-1 text-base text-gray-700">Harga</h4>
+            <h3 className="text-sm font-semibold">
+              {new Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR",
+                maximumSignificantDigits: 3,
+              }).format(data.price)}
+            </h3>
+          </div>
         </div>
       </div>
     </>
